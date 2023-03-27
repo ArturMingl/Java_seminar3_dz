c6dba00da2f64ee3267dd942e56f14452f1d6903
ArturMingl <adiscov90@gmail.com>
2023-03-27 16:05:54 +0300

    first commit

---
+++ task2.java
@@ -0,0 +1,7 @@
+package java_tasks.Seminar3dz;
+
+//*Пусть дан произвольный список целых чисел, удалить из него чётные числа */
+
+public class task2 {
+   
+}
---
+++ task3.java
@@ -0,0 +1,8 @@
+package java_tasks.Seminar3dz;
+
+//*Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф. из этого списка. */
+
+
+public class task3 {
+    
+}
